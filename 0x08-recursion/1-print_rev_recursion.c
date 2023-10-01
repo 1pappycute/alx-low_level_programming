4faea0af41e76e0a260b26b5b8a0c3779d77bee2
Tesleem <hassantesleem52@gmail.com>
2023-10-01 11:00:54 -0700

    1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * _print_rev_recursion  - function to print reverse string
+ * @s: given string
+ * Return Always 0
+*/
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		putchar('\n');
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	putchar(*s);
+}
